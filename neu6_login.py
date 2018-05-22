2e13ae593fe1faee3045fb9b9feda67fec2b1154
Chan <ElvinChan0644@outlook.com>
2018-05-22 19:07:26 +0800

    Update neu6_login.py

--- neu6_login.py
+++ neu6_login.py
@@ -5,14 +5,13 @@ import requests
 from bs4 import BeautifulSoup
 
 url_start = 'http://bt.neu6.edu.cn/member.php?mod=logging&action=login&referer=http%3A%2F%2Fbt.neu6.edu.cn%2Fforum.php'
-# member.php?mod=logging&action=login&loginsubmit=yes&loginhash=LKIV3&inajax=1
 url_login = 'http://bt.neu6.edu.cn/'
 url_main = 'http://bt.neu6.edu.cn/forum.php'
 url_test = 'http://bt.neu6.edu.cn/home.php?mod=spacecp'
 
 
 class neu6():
-    def __init__(self):
+    def __init__(self, username, password):
         headers = {
             "Accept": "text/html, application/xhtml+xml, application/xml",
             "Accept-Encoding": "gzip, deflate",
@@ -25,6 +24,8 @@ class neu6():
         }
         self.session = requests.Session()
         self.session.headers.update(headers)
+        self.username = username
+        self.password = password
 
         r = self.session.get(url_start)
         soup = BeautifulSoup(r.content, 'html.parser')
@@ -34,30 +35,40 @@ class neu6():
         # print(self.action)
 
     def login(self):
-        formdata = {
-            "formhash": self.formhash,
-            "referer": url_main,
-            "username": "",
-            "password": "",
-            "questionid": "0",
-            "answer": ""
-        }
-        self.session.post(url_login+self.action+'&inajax=1', data=formdata)
-        r1 = self.session.get(url_main)
-        r2 = self.session.get(url_test)
-        soup = BeautifulSoup(r2.content, 'html.parser')
-        # print(soup.prettify())
-        points = soup.find_all("a", {"class": "showmenu"})[
-            0].get_text().strip()
-        level = soup.find_all("a", {"class": "xi2"})[
-            0].get_text().strip()
-        print(points)
-        print("用户组:", level)
+        try:
+            formdata = {
+                "formhash": self.formhash,
+                "referer": url_main,
+                "username": self.username,
+                "password": self.password,
+                "questionid": "0",
+                "answer": ""
+            }
+            self.session.post(url_login+self.action+'&inajax=1', data=formdata)
+            r1 = self.session.get(url_main)
+        except:
+            print("登录失败")
+
+    def info(self):
+        try:
+            r2 = self.session.get(url_main)
+            soup = BeautifulSoup(r2.content, 'html.parser')
+            name = soup.find_all("strong", {"class": "vwmy"})[0].get_text()
+            points = soup.find_all("a", {"class": "showmenu"})[
+                0].get_text().strip()
+            level = soup.find_all("a", {"class": "xi2"})[
+                0].get_text().strip()
+            print("用户名:", name)
+            print(points)
+            print("用户组:", level)
+        except:
+            print("获取信息失败")
 
 
 def main():
-    neu = neu6()
+    neu = neu6('username', 'passowrd')
     neu.login()
+    neu.info()
 
 
 if __name__ == '__main__':
